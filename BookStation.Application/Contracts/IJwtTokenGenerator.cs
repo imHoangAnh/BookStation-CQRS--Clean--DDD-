cb3e8899dc7520852c22624516d0d7d4352f519c
HoangAnh <trhoanganh2503@gmail.com>
2026-01-22 17:07:13 +0700

    Update

--- BookStation.Application/Contracts/IJwtTokenGenerator.cs
+++ BookStation.Application/Contracts/IJwtTokenGenerator.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace BookStation.Application.Contracts
+namespace BookStation.Application.Contracts;
+
+public interface IJwtTokenGenerator
 {
-    internal interface IJwtTokenGenerator
-    {
-    }
+    string GenerateToken(long userId, string email);
 }
